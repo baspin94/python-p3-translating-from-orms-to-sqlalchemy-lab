b895215403692193007cd7a50bba061b3d31110b
Bianca Aspin <bianca@mj94.net>
2023-03-05 13:45:29 -0800

    Complete lab.

--- lib/dog.py
+++ lib/dog.py
@@ -1,27 +1,31 @@
 from sqlalchemy import create_engine
+from models import Dog
 
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
-    pass
+    return engine
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
 
 def new_from_db(session):
-    pass
+    return session.query(Dog).first()
 
 def get_all(session):
-    pass
+    return session.query(Dog)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
-
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
